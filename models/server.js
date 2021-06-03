ca2ceaf753800a54a3813820d471dd3c3d0d535e
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-02 22:00:03 -0500

    socket server

--- models/server.js
+++ models/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const { createServer } = require('http');
+const { socketController } = require('../sockets/controllers');
 
 const { dbConnection } = require('../database/config');
 
@@ -9,6 +11,10 @@ class Server {
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+
+        this.server = createServer( this.app );
+        this.io     = require( 'socket.io' )(this.server);
+
 
         this.paths = {
             auth:       '/api/auth',
@@ -28,6 +34,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // sockets
+        this.sockets();
     }
 
     async conectarDB() {
@@ -66,11 +75,17 @@ class Server {
         
     }
 
+    sockets(){
+        this.io.on("connection", socketController );
+    }
+
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
+
+
 
 }
 
