020cdbc60c7a457e09a3079019904de52dfa89d9
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-04 21:40:38 -0500

    auth.js update

--- public/js/auth.js
+++ public/js/auth.js
@@ -4,7 +4,42 @@
 
         const url = ( window.location.hostname.includes('localhost') )
                     ? 'http://localhost:8080/api/auth/'
-                    : 'https://restserver-curso-fher.herokuapp.com/api/auth/';
+                    : 'https://restserver-curso-fher.herokuapp.com/api/auth/'
+                    ;
+
+
+        miFormulario.addEventListener( 'submit', ev => {
+            ev.preventDefault();
+
+            const formData = {};
+
+            for( let el of miFormulario.elements ){
+
+                if( el.name.length > 0 )
+                    formData[el.name] = el.value;
+            
+            }
+            
+
+
+
+            fetch( url + 'login' , {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify( formData )
+            } )
+            .then( resp => resp.json() )
+            .then( ({ msg, token }) => {
+                if( msg ){ return console.error( msg ); }
+                localStorage.setItem( 'token', token );
+            } )
+            .catch( err => console.log( err ) );
+
+            console.log( formData );
+
+        } );
+
+
 
 
         function onSignIn(googleUser) {
